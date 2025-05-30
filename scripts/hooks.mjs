c1124bcea69456463b9c6e39aafaf4850ef0758d
Igor Renan <igorenansc@gmail.com>
2025-05-30 14:37:09 -0300

    fix: Updated hooks events listener

--- scripts/hooks.mjs
+++ scripts/hooks.mjs
@@ -4,17 +4,28 @@ import {ItemMacroConfig} from './ItemMacroConfig.mjs';
 import {SystemManager} from "./systems/SystemManager.mjs";
 
 Hooks.on('init', () => {
+  console.log("Initializing");
   SystemManager.registerHandlers();
   settings.register();
 });
 
 Hooks.on('ready', () => {
+  console.log("Ready");
   if (SystemManager.instance === null && game.user.isGM) {
     ui.notifications.warn(game.i18n.format("warning.systemNotSupported", {system: game.system.title}));
   }
   helper.register();
 });
 
-Hooks.on('renderItemSheet', ItemMacroConfig._init);
+// Hooks.on('renderItemSheet', ItemMacroConfig._init);
+Hooks.on("render", (app, html, data) => {
+  if (app instanceof ItemSheetV2) { // Check for the v2 Item Sheet
+    console.log("Rendering ItemSheetV2", {app, html, data});
+    ItemMacroConfig._init(app, html, data);
+  }
+});
 
-Hooks.on('getItemDirectoryEntryContext', (html, contextOptions) => helper.addContext(contextOptions, "Directory"));
+// Hooks.on('getItemDirectoryEntryContext', (html, contextOptions) => helper.addContext(contextOptions, "Directory"));
+Hooks.on("getDirectoryEntryContext", (html, entryOptions) => {
+  if (app.documentName === "Item") helper.addContext(entryOptions, "Directory");
+});
